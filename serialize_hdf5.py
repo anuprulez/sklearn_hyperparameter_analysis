790287e576320c76d32c06a1f28fea74137a613f
Anup Kumar <anup.rulez@gmail.com>
2018-08-22 12:04:04 +0200

    Update script

--- serialize_hdf5.py
+++ serialize_hdf5.py
@@ -45,11 +45,15 @@ class SerializeToHDF5:
         Convert the definition of a class to JSON
         """
         with open(self.definition, 'w') as definition:
+            clf = dict()
             clf_definition = dict()
             for item in classifier_members:
-                clf_definition[item] = classifier_dict[item]
-            print(clf_definition)
-            definition.write(json.dumps(clf_definition))
+                if item == 'class_name' or item == 'class_path':
+                    clf[item] = classifier_dict[item]
+                else:
+                    clf_definition[item] = classifier_dict[item]
+            clf['definition'] = clf_definition
+            definition.write(json.dumps(clf))
 
     @classmethod
     def convert_to_hdf5(self, classifier_dict):
@@ -61,24 +65,26 @@ class SerializeToHDF5:
                   type_name = type(val).__name__
                   # Store only arrays (for now)
                   if val is not None and type_name == 'ndarray':
-                      print(dict_item)
-                      print(val.shape)
+                      #print(dict_item)
+                      #print(val.shape)
                       dset = h5file.create_dataset(dict_item, (val.shape), data=np.array(val, dtype=val.dtype.name))
 
     @classmethod
-    def serialize_classifier(self):
+    def serialize_class(self):
         """
         Convert to hdf5
         """
-        clf = SVC()
+        clf = SVC(C=2.0)
         classifier = self.train_model(clf)
-
+        print(clf)
         # Get the attributes of the class object
         print("")
         print("Set the datasets in h5 file")
         classifier_dict = classifier.__dict__
         classifier_members = [attr for attr in classifier_dict.keys() if not attr.startswith("_") and not attr.endswith('_')]
         classifier_members.append("class_name")
+        classifier_members.append("class_path")
+        classifier_dict["class_path"] = classifier.__module__ 
         classifier_dict["class_name"] = classifier.__class__.__name__
         self.convert_to_json(classifier_members, classifier_dict)
         self.convert_to_hdf5(classifier_dict)
@@ -94,15 +100,27 @@ class SerializeToHDF5:
         h5file = h5py.File(self.weights, 'r')
         for key in h5file.keys():
             data = h5file.get(key)
-            print(key)
-            print(data.shape)
+            #print(key)
+            #print(data.shape)
 
     @classmethod
-    def recreate_model(self):
+    def deserialize_class(self):
         """
         Recreate the model using the class definition and weights
         """
-        
+        clf = dict()
+        with open(self.definition, 'r') as clf_definition_file:
+            clf = json.loads(clf_definition_file.read())
+        parameters = clf["definition"]
+        class_name = clf["class_name"]
+        class_path = clf["class_path"]
+        class_module = __import__(class_path, fromlist=[class_name])
+        classifier = getattr(class_module, class_name)
+        classifier_cls = classifier()
+        for key, val in parameters.items():
+            setattr(classifier_cls, key, val)
+        print(classifier_cls)
+
 
 if __name__ == "__main__":
 
@@ -111,6 +129,7 @@ if __name__ == "__main__":
         exit( 1 )
     start_time = time.time()
     serialize_clf = SerializeToHDF5()
-    serialize_clf.serialize_classifier()
+    serialize_clf.serialize_class()
+    serialize_clf.deserialize_class()
     end_time = time.time()
     print ("Program finished in %s seconds" % str( end_time - start_time ))
